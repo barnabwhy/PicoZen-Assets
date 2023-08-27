d97f2996cf91c50a98f0b339845c616b73a85597
Barnaby <22575741+barnabwhy@users.noreply.github.com>
2023-08-27 15:46:11 +0100

    Update pico-icons.ts

--- pico-icons.ts
+++ pico-icons.ts
@@ -10,23 +10,22 @@ async function fetchPicoIcons(region: PicoAppRegion) {
     console.log(`[PICO ${region.toUpperCase()}] Saving app icons...`);
     for (let i = 0; i < packageList.length; i++) {
         let packageName = packageList[i];
+        console.log(`[PICO ${region.toUpperCase()}] Saving app icons... (${i + 1}/${packageList.length}) | ${packageName}`);
 
-        
-    console.log(`[PICO ${region.toUpperCase()}] Saving app icons... (${i+1}/${packageList.length}) | ${packageName}`);
-
-        let res = await fetch(
-            `https://appstore-${region}.picovr.com/api/app/v1/item/info`
-            + `?manifest_version_code=${PICO_OPTS.manifestVerCode}&app_language=${PICO_OPTS.appLang}&device_name=${PICO_OPTS.deviceName}`
-            + `&package_name=${packageName}`,
-            {
-                method: 'POST',
-            });
-        let app = (await res.json()).data;
-        
         try {
+            let res = await fetch(
+                `https://appstore-${region}.picovr.com/api/app/v1/item/info`
+                + `?manifest_version_code=${PICO_OPTS.manifestVerCode}&app_language=${PICO_OPTS.appLang}&device_name=${PICO_OPTS.deviceName}`
+                + `&package_name=${packageName}`,
+                {
+                    method: 'POST',
+                });
+
+            let app = (await res.json()).data;
+
             await fs.writeFile(`pico/${region}/assets/icon/${app.package_name}.png_tmp`, (await fetch(app.icon)).body as any);
             await fs.rename(`pico/${region}/assets/icon/${app.package_name}.png_tmp`, `pico/${region}/assets/square/${app.package_name}.png`);
-        } catch(e) {
+        } catch (e) {
             console.log(`[PICO ${region.toUpperCase()}] Error saving icon for ${app.package_name}`);
             await fs.rm(`pico/${region}/assets/icon/${app.package_name}.png_tmp`);
         }
